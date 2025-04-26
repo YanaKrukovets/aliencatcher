2aa69697edae84077363b13eccf09cc9a9739f52
Yana Krukovets <yanashelli@gmail.com>
2025-04-25 22:09:31 -0400

    background fix

--- pages/game.js
+++ pages/game.js
@@ -13,24 +13,22 @@ export default function Game() {
           <div className="relative w-[570px] h-[100vh] overflow-hidden">
             <div className="absolute top-0 left-0 w-full h-[200vh] scroll-vertical-loop">
               {/* First image */}
-              <div className="w-full h-[100vh]">
+              <div className="w-full h-[100vh] relative">
                 <Image
-                  src="/images/pages/game/background.png"
-                  width={270}
-                  height={540}
+                  src="/images/pages/game/background1.png"
                   alt="space background"
-                  className="w-full h-full object-cover"
+                  fill
+                  className="object-cover"
                 />
               </div>
 
-              {/* Second identical image */}
-              <div className="w-full h-[100vh]">
+              {/* Second identical image for looping */}
+              <div className="w-full h-[100vh] relative">
                 <Image
-                  src="/images/pages/game/background.png"
-                  width={270}
-                  height={540}
+                  src="/images/pages/game/background1.png"
                   alt="space background"
-                  className="w-full h-full object-cover"
+                  fill
+                  className="object-cover"
                 />
               </div>
             </div>
